d2a1818a1ae1aff281617b1adf6d1e01378da4fb
Vishal Mehta <103929700+EightFlix@users.noreply.github.com>
2022-05-12 21:41:07 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -8,10 +8,10 @@ Here Is The Help For My Commands."""
 🤖 Version: 2.0 ⚡ """
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and Bot will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+1. Bot should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
