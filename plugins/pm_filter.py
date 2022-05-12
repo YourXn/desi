72325d6e4317ce40a72ef1e1202f22e443393289
Vishal Mehta <103929700+EightFlix@users.noreply.github.com>
2022-05-12 18:55:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -408,7 +408,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ], [
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('🙋🏻‍♀️ Help', callback_data='help'),
-            InlineKeyboardButton('🙈 About', callback_data='about')
+            InlineKeyboardButton('🦹 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
